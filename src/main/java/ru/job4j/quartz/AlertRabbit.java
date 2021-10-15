cd4067aa13f45deb640e117f51748e612b9a06db
meavv <c.c.c.p@bk.ru>
2021-10-15 16:18:41 +0500

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,50 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(readProperties())
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    public static int readProperties() {
+        Properties properties = new Properties();
+        try (BufferedReader bf = new BufferedReader(new FileReader("src/main/resources/rabbit.properties"))) {
+            properties.load(bf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Integer.parseInt(properties.get("rabbit.interval").toString());
+    }
+
+}
